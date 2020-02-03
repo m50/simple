4fbb4f9f8281b0d6f5de281b7aa9dfa7081f0e87
Marisa Clardy <marisa@clardy.eu>
2020-02-03 17:28:19 -0600

    Add empty files for report generation.

---
+++ src/Reports/JsonReport.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace NotSoSimple\Reports;
+
+final class JsonReport
+{
+    public static function generate(string $file, array $errors): void
+    {
+        //
+    }
+}
