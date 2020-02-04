ef61a8e03dbf605461d0bc81194fd9f3d38c6c88
Marisa Clardy <marisa@clardy.eu>
2020-02-03 23:15:55 -0600

    Current Working Directory utility class.

---
+++ src/DataObjects/Cwd.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace NotSoSimple\DataObjects;
+
+use NotSoSimple\Exceptions\AccessDeniedException;
+
+class Cwd
+{
+    public static function get(): string
+    {
+        $cwd = getcwd();
+        if ($cwd === false) {
+            throw new AccessDeniedException('Unable to access current working directory.', 21);
+        }
+        return $cwd;
+    }
+}
