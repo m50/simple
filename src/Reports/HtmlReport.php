aeb7b768504e10fb146e6f4c962fc2d36579e38a
Marisa Clardy <marisa@clardy.eu>
2020-02-08 17:52:11 -0600

    Prepare HTML Report.

--- src/Reports/HtmlReport.php
+++ src/Reports/HtmlReport.php
@@ -6,6 +6,7 @@ final class HtmlReport
 {
     public static function generate(string $file, array $errors): void
     {
-        //
+        $template = file_get_contents('../../tempaltes/report.tpl.html');
+        $sectionTpl = file_get_contents('../../tempaltes/section.tpl.html');
     }
 }
