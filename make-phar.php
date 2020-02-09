0e5154496c7fd9cd640fc3416a073eb349f47b24
Marisa Clardy <marisa@clardy.eu>
2020-02-09 00:56:39 -0600

    Adjust make-phar script.

--- make-phar.php
+++ make-phar.php
@@ -1,7 +1,6 @@
 <?php
 /** @link https://odan.github.io/2017/08/16/create-a-php-phar-file.html */
 
-ini_set('phar.readonly', 0);
 // The php.ini setting phar.readonly must be set to 0
 $pharFile = 'simple.phar';
 
@@ -17,12 +16,12 @@ if (file_exists($pharFile . '.gz')) {
 $p = new Phar($pharFile);
 
 // creating our library using whole directory
-$p->buildFromDirectory('src/');
+$p->buildFromDirectory('./');
 
 // pointing main file which requires all classes
-$p->setDefaultStub('index.php', '/index.php');
+$p->setDefaultStub('simple', '/simple');
 
 // plus - compressing it into gzip
 $p->compress(Phar::GZ);
 
-echo "$pharFile successfully created";
+echo "$pharFile successfully created\n";
